3b8c821369cf2e5618b509fb77f8dca5b6e06bc9
Emiliano Neira <neiraemiliano23@gmail.com>
2023-12-19 10:49:52 -0300

    tercer entrega

---
+++ README.md
@@ -0,0 +1,3 @@
+Espero este bien el trabajo, me tuve que ayudar de varios lados hay cosas que son
+complicadas todavia de entender bien. espero llegar bien a la entrega Final y terminar de comprender todo.
+muchas Gracias
--- main.js
+++ main.js
@@ -5,12 +5,33 @@ const habitaciones = [
   { nombre: "It's Me Mariouuu", costo: 4500 },
 ];
 
-let carrito = [];
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-function agregarHabitacion(indice) {
-  const habitacion = habitaciones[indice];
+const habitacionesDisponiblesElement = document.getElementById(
+  "habitaciones-disponibles"
+);
+
+habitaciones.forEach((habitacion) => {
+  const habitacionOption = document.createElement("div");
+  habitacionOption.innerText = `${habitacion.nombre} - Precio: $${habitacion.costo}`;
+  habitacionesDisponiblesElement.appendChild(habitacionOption);
+});
+
+const botonesReservaElement = document.getElementById("botones-reserva");
+
+habitaciones.forEach((habitacion) => {
+  const botonReserva = document.createElement("button");
+  botonReserva.innerText = `Reservar ${habitacion.nombre}`;
+  botonReserva.addEventListener("click", () => {
+    agregarAlCarrito(habitacion);
+  });
+  botonesReservaElement.appendChild(botonReserva);
+});
+
+function agregarAlCarrito(habitacion) {
   carrito.push(habitacion);
   mostrarCarrito();
+  actualizarLocalStorage();
 }
 
 function mostrarCarrito() {
@@ -19,25 +40,43 @@ function mostrarCarrito() {
 
   let total = 0;
 
-  carrito.forEach((habitacion) => {
+  carrito.forEach((habitacion, index) => {
     const habitacionElement = document.createElement("div");
-    habitacionElement.innerText = `${habitacion.nombre} - Precio: $${habitacion.costo}`;
+    habitacionElement.innerHTML = `${habitacion.nombre} - Precio: $${habitacion.costo} <button onclick="eliminarDelCarrito(${index})">Eliminar</button>`;
     carritoElement.appendChild(habitacionElement);
-
     total += habitacion.costo;
   });
 
-  const totalElement = document.getElementById("total-carrito");
-  totalElement.innerText = `Total: $${total}`;
+  const totalCarritoElement = document.getElementById("total-carrito");
+  totalCarritoElement.innerText = `Total: $${total}`;
+}
+
+function eliminarDelCarrito(index) {
+  carrito.splice(index, 1);
+  mostrarCarrito();
+  actualizarLocalStorage();
+}
+
+function actualizarLocalStorage() {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
 function finalizarCompra() {
-  const totalCompra = carrito.reduce(
-    (total, habitacion) => total + habitacion.costo,
-    0
-  );
-  alert(`¡Gracias por tu compra! El total es de $${totalCompra}.`);
-
-  carrito = [];
-  mostrarCarrito();
+  if (carrito.length > 0) {
+    let total = carrito.reduce((acc, habitacion) => acc + habitacion.costo, 0);
+    alert(
+      `¡Gracias por su compra! Su reserva ha sido realizada con éxito. Total: $${total}`
+    );
+    carrito = [];
+    mostrarCarrito();
+    actualizarLocalStorage();
+  } else {
+    alert(
+      "No hay habitaciones en el carrito. Por favor, seleccione al menos una habitación para realizar la compra."
+    );
+  }
 }
+
+window.onload = function () {
+  mostrarCarrito();
+};
